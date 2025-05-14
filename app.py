a21c8fa958c637a4a5e5669e15ca0317557be1c8
ramatadibt <ramareddy.tadi@bugendaitech.com>
2025-05-14 10:00:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,10 +46,8 @@ st.set_page_config(
     )
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(current_dir, '.streamlit', 'config.yaml')
-with open(config_path) as file:
-    config = yaml.load(file, Loader=SafeLoader)
+config_yaml = st.secrets["config"]
+config = yaml.safe_load(config_yaml)
 
 authenticator = stauth.Authenticate(
     config['credentials'],
